6334629a16e64b980599d42f3d478bcec4ac77fa
BThomas <bran_thom@yahoo.com>
2023-06-20 15:17:09 -0500

    mainly completed portfolio, some styling needed

--- jobs/views.py
+++ jobs/views.py
@@ -1,7 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Job
+from django.shortcuts import redirect
 
 # Create your views here.
 def homepage(request):
     jobs = Job.objects
     return render(request, 'jobs/home.html',{'jobs':jobs})
+
+def detail(request,job_id):
+    job_detail = get_object_or_404(Job, pk=job_id)
+    return render(request, 'jobs/detail.html',{'job':job_detail})
+
+def external_URL():
+    external_URL = Job.urls
+    return redirect(external_URL)
+    
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -23,6 +23,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', jobs.views.homepage, name='home'),
+    path('jobs/<int:job_id>', jobs.views.detail, name ='detail'),
+    path('redirect/', jobs.views.external_URL, name='redirect')
 ] 
 
 urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
