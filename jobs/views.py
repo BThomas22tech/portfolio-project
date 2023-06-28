bc4eb477173c44ec55491bb2874f98a79b8b2627
BThomas <bran_thom@yahoo.com>
2023-06-28 00:46:18 -0500

    changed home to index.html

--- jobs/views.py
+++ jobs/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import redirect
 # Create your views here.
 def homepage(request):
     jobs = Job.objects
-    return render(request, 'jobs/home.html',{'jobs':jobs})
+    return render(request, 'jobs/index.html',{'jobs':jobs})
 
 def detail(request,job_id):
     job_detail = get_object_or_404(Job, pk=job_id)
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-er%yw0ijwtz4l%q10)qkk_+oq)pn2r&vt)d*7z-3!ksv6gobj-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app','*']
 
 
 # Application definition
@@ -79,16 +79,16 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME':'portfoliodb',
-#         'USER': 'postgres',
-#         'PASSWORD': 'root',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':'portfoliodb',
+        'USER': 'postgres',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
 
 # DATABASES = {
 #     'default': {
